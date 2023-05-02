32a5c9969ba2423a1d54139f57529bd85406b327
Kauã Landi <eu@kaualf.com>
2023-05-01 21:01:46 -0300

    feat: reset migrates

--- prisma/migrations/20230502000020_init/migration.sql
+++ prisma/migrations/20230502000020_init/migration.sql
@@ -1,11 +1,25 @@
 -- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `is_admin` BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Product` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     `category_id` INTEGER NOT NULL,
     `measure_id` INTEGER NOT NULL,
-    `price` INTEGER NOT NULL,
+    `price` DOUBLE NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
