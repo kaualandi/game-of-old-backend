3b5ad70d2392d17401f335ba133ab42e55b87f30
Kauã Landi <58404392+kaualandi@users.noreply.github.com>
2023-05-02 13:35:06 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.enableCors();
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
