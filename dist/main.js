365d6a3d41c03684bc943e084b4a5b72f55171ec
Kauã Landi <eu@kaualf.com>
2023-05-07 10:23:19 -0300

    feat: add cert https urls

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const express_1 = require("express");
+const fs = require("fs");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/al-design-api.kaualf.com/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/al-design-api.kaualf.com/fullchain.pem', 'utf8');
+    const httpsOptions = { key: privateKey, cert: certificate };
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true, httpsOptions });
     app.enableCors();
     app.use((0, express_1.json)({ limit: '50mb' }));
     app.use((0, express_1.urlencoded)({ extended: true, limit: '50mb' }));
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const privateKey = fs.readFileSync(
+    '/etc/letsencrypt/live/al-design-api.kaualf.com/privkey.pem',
+    'utf8',
+  );
+  const certificate = fs.readFileSync(
+    '/etc/letsencrypt/live/al-design-api.kaualf.com/fullchain.pem',
+    'utf8',
+  );
+  const httpsOptions = { key: privateKey, cert: certificate };
+
+  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
   app.enableCors();
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
