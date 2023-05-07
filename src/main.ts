920e21b551fe309fe8036d5bd1f7b031633a9dd3
Kauã Landi <eu@kaualf.com>
2023-05-07 10:32:34 -0300

    feat: remove cert https urls

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
-import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const privateKey = fs.readFileSync(
-    '/etc/letsencrypt/live/al-design-api.kaualf.com/privkey.pem',
-    'utf8',
-  );
-  const certificate = fs.readFileSync(
-    '/etc/letsencrypt/live/al-design-api.kaualf.com/fullchain.pem',
-    'utf8',
-  );
-  const httpsOptions = { key: privateKey, cert: certificate };
-
-  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
