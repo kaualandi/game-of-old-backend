3519ab9b422e22b567aac92a8e2109c7bdf52645
Kauã Landi <eu@kaualf.com>
2023-07-25 22:26:54 -0300

    fix: removed category description from dto

--- src/models/categorys/dto/create-category.dto.ts
+++ src/models/categorys/dto/create-category.dto.ts
@@ -3,7 +3,4 @@ import { IsNotEmpty } from 'class-validator';
 export class CreateCategoryDto {
   @IsNotEmpty()
   name: string;
-
-  @IsNotEmpty()
-  description: string;
 }
--- src/models/categorys/dto/update-category.dto.ts
+++ src/models/categorys/dto/update-category.dto.ts
@@ -3,5 +3,4 @@ import { CreateCategoryDto } from './create-category.dto';
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
   name: string;
-  description: string;
 }
